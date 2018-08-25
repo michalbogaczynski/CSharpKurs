a3d757084df82239766faa2dcde37f4b168a1a00
Michał <Micha@DESKTOP-4Q1HUG6>
2018-08-25 23:35:52 +0200

    odc 7 Tworzenie klasy dziennika studenta.

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan
+        List<float> ratings;
+                
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalcualteAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                //sum = sum + rating; to samo co na dole
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRatig()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+
+    }
+}
