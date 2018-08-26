33d07f94de834db09504709c093eec2839b0c2f5
Michał <Micha@DESKTOP-4Q1HUG6>
2018-08-26 18:42:16 +0200

    odc 10 Działania na obiektach. Zmienne referencyjne.

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+        //Stan
+        List<float> ratings;
+                
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalcualteAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                //sum = sum + rating; to samo co na dole
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        public float GiveMaxRatig()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        { 
+            return ratings.Min();
+        }
+
+    }
+}
---
+++ Odcinek10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+            // po skompilowaniu przypiadana będzie ocena 8 ponieważ do zmiennej diary został przypisany nowy obiekt
+            //diary = new Diary();
+            //diary.AddRating(8);
+
+            //dwie zmienne czyli diary i diary2 przypisane są do osobnych obiektów
+            //Diary diary2 = new Diary();
+            //diary.AddRating(8);
+
+            // w obiekcie beda przypisane 3 oceny, obie zmienne przypisane są do jednego obiektu
+            Diary diary2 = diary;
+            diary.AddRating(8);
+
+        }
+    }
+}
