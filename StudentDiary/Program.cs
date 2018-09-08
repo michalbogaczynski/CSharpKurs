114cc40cf255868a1bbeed94607198020a0a4e33
Michał <Micha@DESKTOP-4Q1HUG6>
2018-09-08 12:39:11 +0200

    odc 21 Dodawanie referencji Speech

--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace StudentDiary
 {
@@ -10,17 +11,21 @@ namespace StudentDiary
     {
         static void Main(string[] args)
         {
-            Diary diary = new Diary();
+            //Diary diary = new Diary();
 
-            diary.AddRating(5);
-            diary.AddRating(8.5f);
-            diary.AddRating(4.7f);
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
 
-            DiaryStatistics stats = diary.ComputeStatistics();
+            //DiaryStatistics stats = diary.ComputeStatistics();
 
-            Console.WriteLine("Średnia ocena: " + stats.AverageGrade);
-            Console.WriteLine("Maksymalna ocena: " + stats.MaxGrade);
-            Console.WriteLine("Minimalna ocena: " + stats.MinGrade);
+            //Console.WriteLine("Średnia ocena: " + stats.AverageGrade);
+            //Console.WriteLine("Maksymalna ocena: " + stats.MaxGrade);
+            //Console.WriteLine("Minimalna ocena: " + stats.MinGrade);
+
+            SpeechSynthesizer synt = new SpeechSynthesizer();
+            synt.Speak("Cześć Michał");
+
             Console.ReadKey();
 
         } 
