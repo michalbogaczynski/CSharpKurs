c3d05a4b043051c24129211a58f72793178780a3
Michał <Micha@DESKTOP-4Q1HUG6>
2018-09-08 19:42:52 +0200

    odc 22. Testy jednostkowe

---
+++ Typy.Tests/DiaryTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Typy.Tests
+{
+    [TestClass]
+    public class DiaryTests
+    {
+        [TestMethod]
+        public void CalculateMaxGrate()
+        {
+            Diary diary = new Diary();
+            diary.AddRating(9f);
+            diary.AddRating(8f);
+            diary.AddRating(7f);
+            diary.AddRating(4f);
+            diary.AddRating(3f);
+            diary.AddRating(10f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+
+            Assert.AreEqual(10f, stats.MaxGrade);
+        }
+
+        [TestMethod]
+        public void PassByValueClass()
+        {
+            Diary diary = new Diary();
+            diary.Name = "Jacek";
+
+            SetName(diary);
+
+            Assert.AreEqual("Marcin", diary.Name);
+        }
+
+
+        [TestMethod]
+        public void PassByValueInt()
+        {
+            int x1 = 5;
+            IncrementNumber(x1);
+            Assert.AreEqual(5, x1);
+        }
+
+
+        [TestMethod]
+        public void PassByRefInt()
+        {
+            int x1 = 5;
+            IncrementNumber(ref x1);
+            Assert.AreEqual(15, x1);
+        }
+
+        private void IncrementNumber(int number)
+        {
+            number = 15;
+        }
+
+        private void IncrementNumber(ref int number)
+        {
+            number = 15;
+        }
+
+        private void SetName(Diary diary)
+        {
+            diary.Name = "Marcin";
+        }
+    }
+}
--- Typy/Diary.cs
+++ Typy/Diary.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Typy
 {
-    class Diary
+    public class Diary
     {
         public Diary()
         {
