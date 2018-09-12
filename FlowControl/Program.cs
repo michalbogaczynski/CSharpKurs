ca9fc5b180ebb9a54e11d3b7f4570ab60df43217
Michał <Micha@DESKTOP-4Q1HUG6>
2018-09-12 22:23:10 +0200

    odc 28 Switch case

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -13,61 +13,79 @@ namespace FlowControl
 
         static void Main(string[] args)
         {
+            //instructionIf();
+            Console.WriteLine("Podaj imie: ");
+            name = Console.ReadLine();
 
-            
+            switch (name)
+            {
+                case "Marcin":
+                    Method1();
+                    break;
+                case "Tomek":
+                    Method2();
+                    Method3();
+                    break;
+                default:
+                    MEthod4();
+                    break;
+            }
 
+
+            Console.ReadKey();
+        }
+
+        private static void instructionIf()
+        {
             Console.WriteLine("Podaj swój wiek: ");
             age = int.Parse(Console.ReadLine());
 
             string test = age > 18 ? "Pełnoletni" : "Dziecko"; // trojargumentowe sprawdzenie
             Console.WriteLine(test);
 
-            //Console.WriteLine("Podaj swoje imie: ");
-            //name = Console.ReadLine();
+            Console.WriteLine("Podaj swoje imie: ");
+            name = Console.ReadLine();
 
-            //if (age <= 10)
-            //{
-            //    if (name == "Marcin")
-            //    {
+            if (age <= 10)
+            {
+                if (name == "Marcin")
+                {
 
-            //    }
-            //    Method1();
-            //}
-            //else if (age <20)
-            //{
-            //    Method2();
-            //}
-            //else if (age < 30)
-            //{
-            //    Method3();
-            //}
-            //else
-            //{
-            //    MEthod4();
-            //}
-
-            Console.ReadKey();
+                }
+                Method1();
+            }
+            else if (age < 20)
+            {
+                Method2();
+            }
+            else if (age < 30)
+            {
+                Method3();
+            }
+            else
+            {
+                MEthod4();
+            }
         }
-
 
         private static void MEthod4()
         {
-            Console.WriteLine("Masz 30 lub więcej lat ");
+            Console.WriteLine("Jesteś w metodzie 4");
         }
 
         private static void Method3()
         {
-            Console.WriteLine("Twój wiek jest większy lub równy 20 a mniejszy od 30 i wynosi " + age);
+            Console.WriteLine("Jesteś w metodzie 3");
         }
 
         private static void Method2()
         {
-            Console.WriteLine("Twój wiek jest większy od 10 a mniejszy od 20 i wynosi " + age);
+            Console.WriteLine("Jesteś w drugiej metodzie");
         }
 
         private static void Method1()
         {
-            Console.WriteLine("Masz 10 lub mniej lat a dokładnie masz " + age);
+            Console.WriteLine("Jesteś w pierwszej metodzie");
         }
     }
 }
